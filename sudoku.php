602f64cf9b6ee5640770615153719a6670db04c1
rafaelmuto <r.nagahama@gmail.com>
2018-08-30 22:05:42 -0300

    adding files

--- sudoku.php
+++ sudoku.php
@@ -12,36 +12,39 @@
   <body>
     <h1>SUDOKUUU</h1>
     <hr>
-    <?php
-    $sudoku = [];
-    function sudo_int($sudo_arr){
-      foreach (range(1,9) as $i) {
-        foreach (range(1,9) as $j) {
-          $sudo_arr[$i][$j] = NULL;
-        }
-      }
-      return $sudo_arr;
-    }
-    $sudoku = sudo_int($sudoku);
-     ?>
     <pre>
     <form class="sudo_input" action="sudoku.php" method="post">
       <select name="sudoku[1][3]">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
+        <option value=1>1</option>
+        <option value=2>2</option>
+        <option value=3>3</option>
+        <option value=4>4</option>
+        <option value=5>5</option>
+        <option value=6>6</option>
+        <option value=7>7</option>
+        <option value=8>8</option>
+        <option value=9>9</option>
         <option value="null" selected> - </option>
       </select>
       <input style=" background-color:black; color:white;" type="submit">
     </form>
     <?php
-      var_dump($_POST);
+    $sudoku = [];
+    function sudo_int($sudo_arr){
+      foreach (range(1,9) as $i) {
+        foreach (range(1,9) as $j) {
+          if(isset($_POST["sudoku"][$i][$j])){
+            $sudo_arr[$i][$j] = $_POST["sudoku"][$i][$j];
+          }
+          else{
+            $sudo_arr[$i][$j] = NULL;
+          }
+        }
+      }
+      return $sudo_arr;
+    }
+    $sudoku = sudo_int($sudoku);
+      var_dump($sudoku);
      ?>
   </body>
 </html>
