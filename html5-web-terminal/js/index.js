92132647cbc7f390a14b5509c38634dd77b66aa5
Rafael Muto <r.nagahama@gmail.com>
2018-11-26 09:44:54 -0200

    HTML5 terminal

--- html5-web-terminal/js/index.js
+++ html5-web-terminal/js/index.js
@@ -1,5 +1,5 @@
 $(function() {
-  
+
   // Set the command-line prompt to include the user's IP Address
   //$('.prompt').html('[' + codehelper_ip["IP"] + '@HTML5] # ');
     $('.prompt').html('[user@HTML5] # ');
@@ -7,16 +7,16 @@ $(function() {
   // Initialize a new terminal object
   var term = new Terminal('#input-line .cmdline', '#container output');
   term.init();
-  
+
   // Update the clock every second
   setInterval(function() {
     function r(cls, deg) {
       $('.' + cls).attr('transform', 'rotate('+ deg +' 50 50)')
     }
     var d = new Date()
-    r("sec", 6*d.getSeconds())  
+    r("sec", 6*d.getSeconds())
     r("min", 6*d.getMinutes())
     r("hour", 30*(d.getHours()%12) + d.getMinutes()/2)
   }, 1000);
-  
+
 });
