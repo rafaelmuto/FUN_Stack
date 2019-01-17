5ffbb73a9c429473910dd505c7b18a422188ad20
Rafael Muto <r.nagahama@gmail.com>
2019-01-17 17:24:20 -0200

    ghibli tutorial end

--- GhibliAPI/js/ghibli.js
+++ GhibliAPI/js/ghibli.js
@@ -13,10 +13,31 @@ request.onload = function () {
 
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => {
-            console.log(movie.title);
+            // Create a div with a card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            // Create an h1 and set the text content to the film's title
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            // Create a p and set the text content to the film's description
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 300); // Limit to 300 chars
+            p.textContent = `${movie.description}...`; // End with an ellipses
+
+            // Append the cards to the container element
+            container.appendChild(card);
+
+            // Each card will contain an h1 and a p
+            card.appendChild(h1);
+            card.appendChild(p);
         });
     } else {
         console.log('error');
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
     }
 }
 
