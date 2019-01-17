01c0771f587ccee0f66973e3530ae78b3a8425a6
Rafael Muto <r.nagahama@gmail.com>
2019-01-17 17:13:44 -0200

    update...

---
+++ GhibliAPI/ghibli.js
@@ -0,0 +1,35 @@
+console.log('ghibli.js runnig...');
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function () {
+
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(movie => {
+            console.log(movie.title);
+        });
+    } else {
+        console.log('error');
+    }
+}
+
+// Send request
+request.send();
+
+const app = document.getElementById('root');
+const logo = document.createElement('img');
+
+logo.src = 'Files/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
